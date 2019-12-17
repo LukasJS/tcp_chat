1d66b52e749deab6eeceebb41838046a5605e6e5
Lukas Suess <lsuess97@gmail.com>
2019-12-17 12:45:43 -0800

    Update myClient.c

--- myClient.c
+++ myClient.c
@@ -222,7 +222,7 @@ void printListPackets(int socketNum, uint8_t* packet){
 	  //for(count = 0; count < numHandles; count++) {
     flag = 12;
     while(flag == 12){
-        recv(socketNum, packet, MAXBUF, MSG_DONTWAIT);
+        recv(socketNum, packet, MAXBUF, MSG_WAITALL);
         //printf("Count: %d\n", count);
         //recvFromServer(socketNum, packet);
         printf("Flag: %d\n", flag);
