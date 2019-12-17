0e9a2fdbc01c0870a716bf658228c1417100416e
Lukas Suess <lsuess97@gmail.com>
2019-12-17 12:43:45 -0800

    Update myClient.c

--- myClient.c
+++ myClient.c
@@ -225,6 +225,7 @@ void printListPackets(int socketNum, uint8_t* packet){
         recv(socketNum, packet, MAXBUF, MSG_DONTWAIT);
         //printf("Count: %d\n", count);
         //recvFromServer(socketNum, packet);
+        printf("Flag: %d\n", flag);
         flag = *(packet+sizeof(uint16_t));
         handLen = *(packet+sizeof(struct chat_header));
         printf("%d %.*s\n", count, handLen, packet+4);
