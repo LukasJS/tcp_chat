3e2e52f5da22a0a71e73615af97abbac6b96c862
Lukas Suess <lsuess97@gmail.com>
2019-12-19 14:27:27 -0800

    Update myClient.c

--- myClient.c
+++ myClient.c
@@ -234,7 +234,7 @@ void printListPackets(int socketNum, uint8_t* packet){
     //flag = 12;
     //while(flag == 12){
         //printf("PacketLen recv\n");
-        recv(socketNum, packetLen, 2, MSG_PEEK);
+        printf("RECV Return Len: %d\n", (int)recv(socketNum, packetLen, 2, MSG_PEEK));
 
         uint8_t packetBuf[(size_t)*packetLen];
         printf("packetLen Recieved: %d\n", (int)*packetLen);
