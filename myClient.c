6d928951cb3d899823c96d116f9986f6c7297a9e
Lukas Suess <lsuess97@gmail.com>
2019-12-19 12:37:58 -0800

    Update myClient.c

--- myClient.c
+++ myClient.c
@@ -224,6 +224,7 @@ void printListPackets(int socketNum, uint8_t* packet){
     uint16_t numHandles;
     uint8_t handLen;
     uint8_t packetLen[2];
+    //uint8_t listingTrack[100];
 
     numHandles = *(packet+sizeof(struct chat_header));
     flag = *(packet+sizeof(uint16_t));
@@ -234,8 +235,10 @@ void printListPackets(int socketNum, uint8_t* packet){
     flag = 12;
     while(flag == 12){
         recv(socketNum, packetLen, 2, MSG_PEEK);
+        //listingTrack += packetLen;
         printf("packetLen Recieved: %d\n", (int)*packetLen);
-        printf("RECV Return: %d\n", (int)recv(socketNum, packet, (size_t)packetLen, 0));
+        //printf("packetLen Recieved: %d\n", (int)*listingTrack);
+        printf("RECV Return: %d\n", (int)recv(socketNum, packet, (size_t)packetLen, MSG_PEEK));
         //printf("Count: %d\n", count);
         //recvFromServer(socketNum, packet);
         flag = *(packet+sizeof(uint16_t));
